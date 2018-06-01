545ae3b29093619e4a19dc2ef0c97165223196de
Nurzhan <nurzhan1990@gmail.com>
2018-06-01 16:33:02 +0500

    My first vue project

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="training">
+    <h1>Math training</h1>
+    <hr>
+    <app-start-screen v-if="state == 'start'"></app-start-screen>
+    <app-question v-else-if="state == 'question'"></app-question>
+    <app-message v-else-if="state == 'message'"></app-message>
+    <app-result-screen v-else-if="state == 'results'"></app-result-screen>
+    <div v-else>Unkown state</div> 
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      state: 'start'
+    }
+  }
+}
+</script>
+
+<style scoped>
+	.training{
+		max-width: 700px;
+		margin: 20px auto;
+	}
+</style>
